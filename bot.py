5cce021961fcbf7d285226cefff822c4aef6c2a9
SmoQx <smoqxpl@gmail.com>
2023-09-19 12:56:20 +0200

    Added bot connection and start. Added file which will be responsible for music handling.

--- bot.py
+++ bot.py
@@ -1,4 +1,37 @@
 import discord
+import responses
 
-if __name__ == '__main__':
-    pass
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.response_message(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    token = ' '
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username}, said {user_message}, on channel {channel}')
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(token)
