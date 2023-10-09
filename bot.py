06e53bd6bd9a07d0676914d7b60e50be62f0b829
SmoQx <smoqxpl@gmail.com>
2023-10-09 12:56:21 +0200

    added a splay function which is configured to play music from spotify unfortunately it only plays the preview

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ from spotipy.oauth2 import SpotifyOAuth
 from urllib.parse import urlparse
 
 
+semaphore = asyncio.Semaphore(1)
+
+
 async def send_message(message, user_message, is_private):
     try:
         response = responses.response_message(user_message)
@@ -22,6 +25,24 @@ def is_valid_url(url):
         return all([result.scheme, result.netloc])
     except ValueError:
         return False
+
+
+def search_youtube_videos(query):
+    ydl_opts = {
+        'format': 'best',  # You can adjust the format as needed
+        'extract_flat': True,  # Extract only flat results (videos)
+        'quiet': True,  # Suppress yt-dlp output
+    }
+
+    with yt.YoutubeDL(ydl_opts) as ydl:
+        search_results = ydl.extract_info(f'ytsearch:{query}', download=False)
+
+        # Check if 'entries' key exists in the result (videos found)
+        if 'entries' in search_results:
+            videos = search_results['entries']
+            return videos
+        else:
+            return []
 
 
 def run_discord_bot():
@@ -41,9 +62,9 @@ def run_discord_bot():
     }
 
     # Initialize the Spotify API client
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='YOUR_SPOTIFY_CLIENT_ID',
-                                                   client_secret='YOUR_SPOTIFY_CLIENT_SECRET',
-                                                   redirect_uri='YOUR_REDIRECT_URI',
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='fbe4ddd2b0944d8caadd82fc3a634ba6',
+                                                   client_secret='937f97778b604fa8a3367d81979d23c8',
+                                                   redirect_uri='http://127.0.0.1:8888/callback',
                                                    scope='user-library-read user-read-playback-state user-modify-playback-state'))
 
     async def play_next(ctx):
@@ -92,26 +113,40 @@ def run_discord_bot():
         await ctx.send(f'Songs in q: \n {title}')
 
     @client.command()
-    async def play(ctx, url):
-        play_queue.append(url)
-        channel = ctx.author.voice.channel
+    async def play(ctx, *url):
+        async with semaphore:
+            channel = ctx.author.voice.channel
 
-        if ctx.voice_client is None:
-            await channel.connect()
-        else:
-            print("already connected")
+            if ctx.voice_client is None:
+                await channel.connect()
+            else:
+                print("already connected")
 
-        if is_valid_url(url):
-            print(f"IS VALID URL{url}")
-        else:
-            await ctx.send("Its not an URL")
+            if is_valid_url(url[0]):
+                play_queue.append(url[0])
+                print(f"IS VALID URL{url[0]}")
+                if not ctx.voice_client.is_playing():
+                    await play_next(ctx)
+                else:
+                    with yt.YoutubeDL(ydl_opts) as ydl:
+                        info = ydl.extract_info(url[0], download=False)
+                    await ctx.send(f"Added to q: {info['title']}.")
+            else:
+                await ctx.send("Its not an URL")
+                # Example usage:
+                for string in url:
+                    print(string)
+                query = url
+                videos = search_youtube_videos(query)
 
-        if not ctx.voice_client.is_playing():
-            await play_next(ctx)
-        else:
-            with yt.YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(url, download=False)
-            await ctx.send(f"Added to q: {info['title']}.")
+                if videos:
+                    for video in videos:
+                        print(f"Video Title: {video['title']}")
+                        print(f"Video URL: https://www.youtube.com/watch?v={video['id']}")
+                        play_queue.append(f"https://www.youtube.com/watch?v={video['id']}")
+                        await play_next(ctx)
+                else:
+                    print("No videos found for the given query.")
 
     @client.command()
     async def skip(ctx):
@@ -141,26 +176,6 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
-    """
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        if message.author.bot:
-            return
-
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-
-        print(f'{username}, said {user_message}, on channel {channel}')
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
-            if responses.response_message(user_message) == 'Now playing! ':
-                print(message)"""
 
     @client.command()
     async def join(ctx):
@@ -186,19 +201,39 @@ def run_discord_bot():
 
     @client.command()
     async def splay(ctx, query):
-        # Search for a track on Spotify
-        results = sp.search(q=query, limit=1, type='track')
+        async with semaphore:
+            # Check if the bot is in a voice channel
+            if ctx.voice_client is None:
+                await ctx.send('I am not in a voice channel. Use `!join` to bring me into a channel.')
+                return
 
-        if results['tracks']['items']:
+            if ctx.voice_client.is_playing():
+                pass
+
+            # Use Spotipy to search for tracks based on the query
+            results = sp.search(query, limit=1, type='track')
+
+            # Check if any tracks were found
+            if len(results['tracks']['items']) == 0:
+                await ctx.send('No tracks found.')
+                return
+            with open('bot.txt', 'w') as save:
+                save.write(str(results))
+
+            # Get the first track found
             track_uri = results['tracks']['items'][0]['uri']
 
-            # Join the voice channel
-            channel = ctx.author.voice.channel
-            vc = await channel.connect()
+            # Get the audio stream URL for the Spotify track
+            track_info = sp.track(track_uri)
+            audio_url = track_info['preview_url']
 
-            # Play the Spotify track
-            vc.play(discord.FFmpegPCMAudio(track_uri))
-        else:
-            await ctx.send("Track not found.")
+            if audio_url is None:
+                await ctx.send('The selected track does not have a preview available.')
+                return
+
+            # Play the audio URL in the voice channel
+            ctx.voice_client.stop()
+            ctx.voice_client.play(discord.FFmpegPCMAudio(audio_url))
+            await ctx.send(f'Now playing: {results["tracks"]["items"][0]["name"]}')
 
     client.run(token)
