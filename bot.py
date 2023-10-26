764761c6f5833bc4a5816449fd015afd562397ef
SmoQ <smoku250@gmail.com>
2023-10-26 17:16:17 +0200

    deleted token

--- bot.py
+++ bot.py
@@ -67,7 +67,7 @@ async def time_and_print_execution_time(func):
 def run_discord_bot():
     intents = discord.Intents.default()
     intents.message_content = True
-    token = 'NjkwOTkyNjM4NjM5MzQxNjI5.GXNezh.MBI7BhO6auPjtbwYq6orlA07voB73Ar4MMGdv8'
+    token = 'token_here'
     client = commands.Bot(command_prefix='!', intents=intents)
     play_queue = []
     audio_files_list = []
