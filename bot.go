8ff9d93f16ce7f479649055a5bd05f56a2bb6b1f
smoqx <smoqxpl@gmail.com>
2026-04-19 12:40:42 +0200

    change back

--- bot.go
+++ bot.go
@@ -165,7 +165,7 @@ func voiceStateUpdate(s *discordgo.Session, vs *discordgo.VoiceStateUpdate) {
 	}
 
 	// If no one else is left, disconnect
-	if memberCount == 1 {
+	if memberCount == 0 {
 		fmt.Println("No one left, leaving channel.")
 		vc.Disconnect(context.TODO())
 		delete(voiceConnections, vs.GuildID)
