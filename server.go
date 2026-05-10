af58268443e7bc0ae6f741a12f80ac4beac205c0
smoqxpl <smoqxpl@gmail.com>
2026-05-10 19:26:46 +0200

    change proxy

--- server.go
+++ server.go
@@ -239,7 +239,7 @@ func handleCallback(c *gin.Context) {
 	session.Save()
 
 	// redirect to frontend
-	c.Redirect(http.StatusTemporaryRedirect, ":8080")
+	c.Redirect(http.StatusTemporaryRedirect, ":69696")
 }
 
 // middleware — protects all /api routes
@@ -463,5 +463,5 @@ func RunServer(db *sql.DB) {
 		ctx.HTML(http.StatusOK, "discord-music-bot.html", nil)
 	})
 
-	router.Run(":8080")
+	router.Run(":69696")
 }
