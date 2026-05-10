e7a2873174d0ef42cecdd5b63b483e579151673e
smoqxpl <smoqxpl@gmail.com>
2026-05-10 19:55:12 +0200

    removed handler

--- bot.go
+++ bot.go
@@ -70,7 +70,7 @@ func Run(token string, db *sql.DB) {
 	// 	newMessage(s, m, db) // pass db yourself
 	// })
 
-	discord.AddHandler(voiceStateUpdate)
+	// discord.AddHandler(voiceStateUpdate)
 
 	discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		newCommand(s, i, db)
